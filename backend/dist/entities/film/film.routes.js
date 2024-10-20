9fc0c8a935a68b74624bece04a6b14e0b19c0a20
Lucas Perez <89951351+LucasPerezz@users.noreply.github.com>
2024-10-20 00:25:33 -0300

    Delete backend/dist directory

--- backend/dist/entities/film/film.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const film_controller_1 = require("./film.controller");
-const filmRouter = (0, express_1.Router)();
-filmRouter.get("/", film_controller_1.getFilms);
-filmRouter.get("/:title", film_controller_1.getFilmByTitle);
-exports.default = filmRouter;
--- backend/dist/entities/planet/planet.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const planet_controller_1 = require("./planet.controller");
-const planetsRouter = (0, express_1.Router)();
-planetsRouter.get("/", planet_controller_1.getPlanets);
-planetsRouter.get("/:name", planet_controller_1.getPlanetsByName);
-exports.default = planetsRouter;
